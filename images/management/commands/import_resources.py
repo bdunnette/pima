897c03b4cad923a487283b72751f6111463d64b7
Brian Dunnette <dunn0172@umn.edu>
2013-07-18 11:40:52 -0500

    import script now reads javascript variables into dicts; still need code to process these into cases/images

--- images/management/commands/import_resources.py
+++ images/management/commands/import_resources.py
@@ -7,11 +7,26 @@ class Command(BaseCommand):
     help = 'Imports images from the specified directories'
 
     def handle(self, *args, **options):
+	data = {}
+	data2 = {}
         for dir in args:
+	    var_name = ''
             try:
-	        js_file = open('/'.join([dir, 'data.js']))
-		print js_file.read()
-		print json.loads(js_file.read())
+		js_file = open('/'.join([dir, 'data.js']))
+		for line in js_file.readlines():
+		    print line
+	 	    if line.strip()[:3] == 'var':
+			data[var_name] = data2
+		        var_name = line[3:].split()[0]
+			data[var_name] = {}
+			data2 = {}
+		    else:
+			vals = line.translate(None,"{}[]\t\r\n").split(",")
+			if len(vals) > 1:
+			    print vals, var_name
+			    data2[vals[0].strip(" '")] = vals
+		for datum in data:
+		    print datum, data[datum]
 	    except Exception as inst:
 		print type(inst)     # the exception instance
 		print inst.args      # arguments stored in .args
