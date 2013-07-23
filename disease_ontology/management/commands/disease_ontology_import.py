fd6865a0688d33d7741527cf21b805a0358a7116
Brian Dunnette <dunn0172@umn.edu>
2013-07-23 16:08:24 -0500

    importing from OBO file instead of RDF

--- disease_ontology/management/commands/disease_ontology_import.py
+++ disease_ontology/management/commands/disease_ontology_import.py
@@ -2,6 +2,12 @@ from django.core.management.base import BaseCommand, CommandError
 from disease_ontology.models import Term
 import pprint
 
+def split_line(line):
+    line_split = line.split(':', 1)
+    line_type = line_split[0]
+    line_val = line_split[1]
+    return line_type, line_split
+
 class Command(BaseCommand):
     args = '<dir dir ...>'
     help = 'Imports images from the specified directories'
@@ -11,8 +17,11 @@ class Command(BaseCommand):
             do_file = open('disease_ontology/HumanDO.obo')
             do_data = do_file.read().split('\n\n')
             for datum in do_data:
-                datum_type = datum.split('\n')[0]
-                print datum_type
+                datum_split = datum.split('\n')
+                datum_type = datum_split[0]
+                if datum_type == '[Term]':
+                    for line in datum_split[1:]:
+                        print split_line(line)
         except Exception as inst:
             print type(inst)     # the exception instance
             print inst.args      # arguments stored in .args
