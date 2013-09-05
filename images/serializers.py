be0d68fc8d7b804ff82c0077972481e354c859ae
Brian Dunnette <dunn0172@umn.edu>
2013-09-05 10:05:04 -0500

    fixing serializers

--- images/serializers.py
+++ images/serializers.py
@@ -4,7 +4,9 @@ from rest_framework import serializers
 class CaseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Case
+        fields = ('url', 'title', 'diagnosis', 'resources')
 
 class ResourceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Resource
+        fields = ('url', 'description', 'diagnosis', 'file')
